5db1d8a3ec50bccfd04445a436388d1aed804209
Konstantinos Bozikis <konstantinos.bozikis@yahoo.com>
2019-10-13 14:27:34 +0300

    order progress labels

--- SmartBookStore/OrderProgress.Designer.cs
+++ SmartBookStore/OrderProgress.Designer.cs
@@ -39,6 +39,10 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -154,11 +158,59 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Επεξεργασία";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Συλλογή προϊόντων";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(419, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Προς παράδοση";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(567, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Παραδόθηκε";
+            this.label5.Visible = false;
+            // 
             // OrderProgress
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pictureBox6);
@@ -195,5 +247,9 @@
         private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- SmartBookStore/OrderProgress.cs
+++ SmartBookStore/OrderProgress.cs
@@ -14,7 +14,9 @@ namespace SmartBookStore
     {
         public Form form;
         int i = 0;
+        int j = 0;
         List<PictureBox> pictures = new List<PictureBox>();
+        List<Label> labels = new List<Label>();
         public OrderProgress(Form form)
         {
             InitializeComponent();
@@ -23,18 +25,22 @@ namespace SmartBookStore
             pictureBox5.ImageLocation = "OrderProgress/shipped.jpg";
             pictureBox7.ImageLocation = "OrderProgress/delivered.jpg";
             var pictures = new List<PictureBox> { pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7};
+            var labels = new List<Label> {label2, label3, label4, label5};
             this.pictures = pictures;
+            this.labels = labels;
             this.form = form;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             pictures[i].Visible = true;
+            labels[j].Visible = true;
             if (i > 0)
             {
-                pictures[i - 1].Visible = true;
+                pictures[i - 1].Visible = true;              
             }
             i += 2;
+            j++;
             if (i > 6)
             {
                 timer1.Stop();
