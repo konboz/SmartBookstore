b3935c8dd967f4c7b2772932ca05a587ddf9af45
Konstantinos Boz <kotsos_573@hotmail.com>
2019-10-14 19:36:55 +0300

    fh

--- SmartBookStore/Cafe.Designer.cs
+++ SmartBookStore/Cafe.Designer.cs
@@ -116,6 +116,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Cafe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cafe";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
